48a6d58c5038df7d8a1a492456e9c797b0722e67
IT21314742 <it21314742@my.sliit.lk>
2024-08-23 18:46:19 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 img = cv2.imread('assets/chessboard.png')
-img = cv2.resize(img, (0, 0), fx=0.75, fy=)
+img = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)
 
 
 cv2.imshow('frame', img)
