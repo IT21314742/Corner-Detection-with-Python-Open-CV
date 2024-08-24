224c687a399e5fa735815c207f8fbb8d8431d91d
IT21314742 <it21314742@my.sliit.lk>
2024-08-24 16:02:20 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ img = cv2.imread('assets/chessboard.png')
 img = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
+corners
 
 cv2.imshow('frame', img)
 cv2.waitKey(0)
