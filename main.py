ce8eafe20d199ce85b447faa5a9942b26fd19cb3
IT21314742 <it21314742@my.sliit.lk>
2024-08-25 16:26:11 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ img = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 corners = cv2.goodFeaturesToTrack(img, 100, 0.01, 10)
-corners 
+corners =
 
 
 cv2.imshow('frame', img)
