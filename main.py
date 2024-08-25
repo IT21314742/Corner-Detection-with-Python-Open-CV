56b4c329d6300fcd0aaece041ef33c148a9259e7
IT21314742 <it21314742@my.sliit.lk>
2024-08-25 16:55:56 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ img = cv2.imread('assets/chessboard.png')
 img = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-corners = cv2.goodFeaturesToTrack(img, 100, 0.01, 10)
+corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 10)
 corners = np.int0(corners)
 
 for corner in corners:
