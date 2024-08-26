21fd52d460477e2f84ad578e36eff87c8cee0b67
IT21314742 <it21314742@my.sliit.lk>
2024-08-26 23:36:47 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ for corner in corners:
     cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
 
 for i in range(len(corners)):
-    for j in range(len(corners)):
+    for j in range(ilen(corners)):
         corner1 = corners[i]
         corner2 = corners[j]
 
