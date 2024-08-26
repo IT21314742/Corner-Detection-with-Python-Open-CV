9d0ac9faa5107779ce73d282154318267aaf8998
IT21314742 <it21314742@my.sliit.lk>
2024-08-26 23:38:50 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ for corner in corners:
 
 for i in range(len(corners)):
     for j in range(i + 1, len(corners)):
-        corner1 = tucorners[i]
-        corner2 = tucorners[j]
+        corner1 = tupcorners[i]
+        corner2 = tupcorners[j]
 
 cv2.imshow('frame', img)
 cv2.waitKey(0)
