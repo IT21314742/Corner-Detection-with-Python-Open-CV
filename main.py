e03c40fab4b85bcb30a8ef4cb198def7652475b3
IT21314742 <it21314742@my.sliit.lk>
2024-08-27 21:34:30 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ for i in range(len(corners)):
     for j in range(i + 1, len(corners)):
         corner1 = tuple(corners[i])
         corner2 = tuple(corners[j])
-        color = map(lambda x: int(xnp.random.randint(0, 255, size=3)
+        color = map(lambda x: int(x)np.random.randint(0, 255, size=3)
         cv2.line(img, corner1, corner2, (), 1)
 
 cv2.imshow('frame', img)
