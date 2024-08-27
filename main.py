a16c1cf7d127ec244fd8812a9fbe45748ab7011d
IT21314742 <it21314742@my.sliit.lk>
2024-08-27 21:15:57 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ for i in range(len(corners)):
     for j in range(i + 1, len(corners)):
         corner1 = tuple(corners[i])
         corner2 = tuple(corners[j])
-        cv2.line(img, corner1, corner2, (), )
+        cv2.line(img, corner1, corner2, (), 1)
 
 cv2.imshow('frame', img)
 cv2.waitKey(0)
